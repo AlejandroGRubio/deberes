643815c338b747b4270a0db72109a85acbd313c9
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-09-22 01:39:33 +0200

    act prueba

--- practica1/ejercicio3.js
+++ practica1/ejercicio3.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function repetirNumero(num1, num2){
-    var number = num2; // mal arreglar, guardar como String
+    var number = `${num2} `; // mal arreglar, guardar como String
     var arrayNum = {};
     for(i = 0; i == num1; i++){
         arrayNum(i)=number;
