e6d418b5d61cc187037dec96ec775e050f3ace41
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-09-21 19:05:40 +0200

    ejercicio3

---
+++ practica1/ejercicio3.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function repetirNumero(num1, num2){
+    var number = num2; // mal arreglar, guardar como String
+    var arrayNum = {};
+    for(i = 0; i == num1; i++){
+        arrayNum(i)=number;
+        number= number*2;
+    }
+}
+
+console.log(`La repeticion de los numeros da como resultado ${repetirNumero(4,6)}`);
