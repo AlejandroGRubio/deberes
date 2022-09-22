52097a13ad1503f72971a71c885d559f16e3c14b
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-09-22 02:06:34 +0200

    arreglo ejer3

--- practica1/ejercicio3.js
+++ practica1/ejercicio3.js
@@ -1,12 +1,15 @@
 "use strict";
 
 function repetirNumero(num1, num2){
-    var number = `${num2}`; // mal arreglar, guardar como String
-    var arrayNum = {};
-    for(i = 0; i == num1; i++){
-        arrayNum(i)=number;
-        number= number*2;
+    let number = num2;
+    let stringNumber = `${num2}`;
+    let i = 1;
+    while (i != num1) {
+        number = number*2;
+        stringNumber = `${stringNumber} ${number}`;
+        i++;
     }
+    return `${stringNumber}`;
 }
 
-console.log(`La repeticion de los numeros da como resultado ${repetirNumero(4,6)}`);
+console.log(`La repetición de los números da como resultado ${repetirNumero(4,6)}`);
