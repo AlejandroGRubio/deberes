124a438e7f415a11be1eeb434d21f552a1c8d529
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-09-22 02:38:00 +0200

    ejercicio 4

--- practica1/ejercicio4.js
+++ practica1/ejercicio4.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function productoConImpuestos(nomProducto = `Producto genérico`, precio = 100, impuesto = 21){
+
+    if (isNaN(precio)) {
+        return `ERROR. El precio insertado no es un valor válido`;
+    }
+    else if (isNaN(impuesto)) {
+        return `ERROR. El impuesto insertado no es un valor válido`;
+    }
+    else{
+
+        return `El producto es ${nomProducto} con un precio final de ${precio + (precio * (impuesto/100))} de euros`;
+
+
+    }
+}
+
+console.log(`${productoConImpuestos("escoba", 7, 21)}`);
