5af46451ab11e00054bf034f739ec0a3420f655d
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-09-29 15:50:48 +0200

    ejercicio3 terminado

--- practica 5/ejercicio2.js
+++ practica 5/ejercicio2.js
@@ -1,26 +1,30 @@
 "use strict";
-
+//Se guarda la id del Intervalo para su posterior uso.
 const id = setInterval(temporizador, 1000);
 var minutos = 1;
 var segundos = 5;
 
 function temporizador() {
     
+    //Comnprueba si solo le has dado un parametro.
    if (minutos >= 0 && segundos == undefined) {
        segundos = minutos;
        minutos = 0;
    }
+   //Comprueba si son números negativos y da error.
    else if (minutos < 0 || segundos < 0) {
-        console.log(`Error. No se pueden usar números negativos.`);
+        return console.log(`Error. No se pueden usar números negativos.`);
    }
+   //Comprueba si los segundos son mayores a 59 y da error.
    else if (segundos > 59){
-        console.log(`Los segundos no pueden ser mayor a 59.`);
+        return console.log(`Los segundos no pueden ser mayor a 59.`);
    }
+   //Comprueba si los valores estan a cero y para el Intervalo.
    else if (minutos == 0 && segundos == 0){
         clearInterval(id);
    }
    
-
+   //Cambia el formato que da por consola para que se vean bien los segundos menor a 10.
     if (segundos < 10) {
         console.log(`${minutos}:0${segundos}`);
     }
@@ -29,7 +33,7 @@ function temporizador() {
     }
 
     segundos--;
-
+    //Cambien el minuto cuando los segundos bajan de 0 y lo vuelve a 59 los segundos.
     if (segundos == -1) {
         segundos = 59;
         if (minutos > 0) {
--- practica 5/ejercicio3.js
+++ practica 5/ejercicio3.js
@@ -1,9 +1,37 @@
 "use strict";
 
+//Guardamos la id del Intervalo para su posterior uso.
+const id = setInterval(pedirDNI, 10000);
 
-const id = setInterval(pedirDNI, 1000);
+var arrayDNI = [];
 
+//Guardamos un array con las letras del DNI para luego usar su posición para identificarlas.
+var letrasDNI = [`T`, `R`, `W`, `A`, `G`, `M`, `Y`, `F`, `P`, `D`, `X`, `B`, `N`, `J`, `Z`, `S`, `Q`, `V`, `H`, `L`, `C`, `K`, `E`];
+
+var imprimirLetrasDNI = ``;
 
 function pedirDNI() {
-    
+    let numDNI = prompt(`Introduce tu número de DNI sin letra: (Introduzca -1 para terminar)`);
+    //Si se introduce -1 entra a esta condicion, que para el Interval.
+    if (numDNI == -1) {
+        clearInterval(id);
+        //Identifica la letra de los números de los DNI guardados.
+        for (let i = 0; i < arrayDNI.length; i++) {
+            let resto = arrayDNI[i] % 23;
+            imprimirLetrasDNI = `${imprimirLetrasDNI} ${letrasDNI[resto]}`;
+            
+        }
+
+        console.log(`Las letras de los DNI introducidos son:${imprimirLetrasDNI}`);
+
+
+    }
+    else{
+        //Guarda los números de DNI en el array.
+        arrayDNI = [...arrayDNI, numDNI];
+
+    }
+
+
+
 }
