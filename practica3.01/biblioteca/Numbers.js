4313c8e372c0e8cb0e80523cef933fe0f1e6cc29
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-10-16 17:33:57 +0200

    comentarios

---
+++ practica3.01/biblioteca/Numbers.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+
+
+export function GenNum() {
+    
+    var num = Math.floor((Math.random() * 1000) +1);
+
+    return num;
+
+
+}
---
+++ practica3.01/ejercicio2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var doc = window.document;
+
+//Genera un número aleatorio de 1 a 1000
+function GenNum() {
+    
+    var num = Math.floor((Math.random() * 1000) +1);
+
+    return num;
+
+
+}
+//Añade el texto con etiqueta que quieras indicando la id de un HTML.
+function anyadirById(id, texto){
+
+    var cont = doc.getElementById(id);
+
+
+    return cont.insertAdjacentHTML(`beforeend`,texto);
+
+
+
+}
+
+//Añade un número dentro de un li.
+const anyadirNum = () => {
+    
+
+    anyadirById(`numeros`, `<li>${GenNum()}</li>`);
+
+
+
+
+}
