ac33a7a84e8fbdf28c611c29e5d6b02d2f48183c
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-10-18 00:29:10 +0200

    comentarios ejer 1

--- practica3.02/bibilioteca/Filtro.js
+++ practica3.02/bibilioteca/Filtro.js
@@ -5,17 +5,19 @@
 var doc = window.document;
 
 
-
+//Como no he podido seleccionar las etiquetas de dentro del body, he tenid que hacerlo un poco más manual, pero esto censura la palabra sexo de dentro de esas etiquetas.
 export function censurarPalabra(palabra) {
     
     var cont = doc.querySelectorAll(`h1`);
     var texto = ``;
 
     for(var item of cont){
-
+     //Selecciona el texto y lo guarda en una variable.
      texto = item.innerHTML;
+     //Luego lo elimina del html.
      item.innerHTML = ``;
-     item.insertAdjacentHTML(`beforeend`,texto.replace(`sexo`, `<strong>Contenido Bloqueado</strong>`))
+     //Y lo vuelve a insertar, solo que reemplazando la palabra por <strong>Contenido Bloqueado</strong>, con el strong para poder modificarlo con css.
+     item.insertAdjacentHTML(`beforeend`,texto.replace(palabra, `<strong>Contenido Bloqueado</strong>`))
 
 
 
@@ -23,10 +25,10 @@ export function censurarPalabra(palabra) {
     var cont = doc.querySelectorAll(`h2`);
 
     for(var item of cont){
-
+     
      texto = item.innerHTML;
      item.innerHTML = ``;
-     item.insertAdjacentHTML(`beforeend`,texto.replace(`sexo`, `<strong>Contenido Bloqueado</strong>`))
+     item.insertAdjacentHTML(`beforeend`,texto.replace(palabra, `<strong>Contenido Bloqueado</strong>`))
 
 
 
@@ -39,7 +41,7 @@ export function censurarPalabra(palabra) {
 
      texto = item.innerHTML;
      item.innerHTML = ``;
-     item.insertAdjacentHTML(`beforeend`,texto.replace(`sexo`, `<strong>Contenido Bloqueado</strong>`))
+     item.insertAdjacentHTML(`beforeend`,texto.replace(palabra, `<strong>Contenido Bloqueado</strong>`))
 
 
 
--- practica3.02/ejercicio1.js
+++ practica3.02/ejercicio1.js
@@ -1,10 +1,10 @@
 "use strict";
 
-
+//Importo la libreria.
 import * as filt from "./bibilioteca/Filtro.js";
 
 
 
-
+//Ejecutamos el filtro.
 filt.censurarPalabra(`sexo`);
 
