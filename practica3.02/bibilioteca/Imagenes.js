c5e0cc4dc49e9e382cf3e9a02aa49c13d76cddb2
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-10-18 13:10:35 +0200

    no sale el ejercicio4

--- practica3.02/bibilioteca/Imagenes.js
+++ practica3.02/bibilioteca/Imagenes.js
@@ -5,9 +5,16 @@ var doc = window.document;
 
 
 
-export function cambiarImagenes(array, tag) {
+export function cambiarImagenes(array, tag, posi) {
     
 
+    var cont = doc.getElementsByTagName(tag);
+
+    if(posi == array.lenght){
+        posi = 0
+    }
+
+    cont[0].src = array[posi];
 
 
 
--- practica3.02/ejercicio4.js
+++ practica3.02/ejercicio4.js
@@ -1,12 +1,44 @@
 "use strict";
 
-
+var doc = window.document;
 
 var imagenes = ["./img/feo.jpg", "./img/fea.jpg", "./img/feos.jpg", "./img/feo.jpg"];
 
+var contar = 0;
+
+setInterval(cambiarImagenes(imagenes, `img`, contar), 2000);
+
+
+function editarContar(limite) {
+    
+    contar++;
+
+    if (contar == limite) {
+        contar = 0;
+    }
+
+
+    return 
+
+}
 
 
 
+function cambiarImagenes(array, tag, num) {
+    
+
+    var cont = doc.getElementsByTagName(tag);
+
+    var largo = array.lenght;
+
+    if(num == largo){
+        contar = 0;
+    }
 
 
+    cont[0].src = array[num];
+
+    editarContar(largo);
+
+}
 
