22a07d9741e8fc80b13a8b0d280c87b1bdcd4a75
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-10-18 19:57:46 +0200

    seguir practica303

--- practica3.03/js/gestor.js
+++ practica3.03/js/gestor.js
@@ -1,1 +1,64 @@
 "use strict";
+
+
+var doc = window.document;
+
+
+function anyadirOnClick() {
+    
+    var cont = doc.getElementsByTagName(`input`);
+
+
+    for (let i = 0; i < cont.length; i++) {
+        
+        
+    }
+
+
+
+}
+
+function gurdarYBorrar() {
+    //guarda en variables el contenido (0) de pendientes y acabadas. 
+}
+
+
+
+
+
+function pasarPendiente(texto) {
+    
+
+    var cont = doc.getElementById(`pendientes`);
+
+    var zonaPendiente = doc.getElementsByClassName(`tarea`);
+
+
+    var tags = zonaPendiente[0].innerHTML;
+
+    cont.insertAdjacentHTML(`beforeend`,`<div class="tarea">${tags.replace(`<p>Texto de la tarea</p>`, `<p>${texto}</p>`)} </div>`);
+
+
+
+
+}
+
+const anyadir = () => {
+
+    var cont = doc.getElementsByClassName(`textarea`);
+
+    console.log(cont[0].value);
+
+    if (cont[0].value == undefined) {
+        cont[0].value = `Escribe algo venga.`;
+        return console.log(`Miras aquí pero no escribes nada. ;-;`);
+    }
+
+
+    pasarPendiente(`${cont[0].value}`);
+
+
+};
+
+
+
