1cb39b3244bae945343a3f3caea7ad94a00c5152
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-10-18 18:57:12 +0200

    ejercicio4 acabado

--- practica3.02/ejercicio4.js
+++ practica3.02/ejercicio4.js
@@ -2,43 +2,46 @@
 
 var doc = window.document;
 
-var imagenes = ["./img/feo.jpg", "./img/fea.jpg", "./img/feos.jpg", "./img/feo.jpg"];
+let imagenes = ["./img/feo.jpg", "./img/fea.jpg", "./img/feos.jpg", "./img/feas.jpg"];
 
-var contar = 0;
+let contar = 0;
 
-setInterval(cambiarImagenes(imagenes, `img`, contar), 2000);
+let opacidad = 1;
 
-
-function editarContar(limite) {
-    
-    contar++;
-
-    if (contar == limite) {
-        contar = 0;
-    }
+setInterval(cambiarImagenes, 2000, `img`);
+setInterval(efectos, 500, `img`);
 
 
-    return 
-
-}
-
-
-
-function cambiarImagenes(array, tag, num) {
+function cambiarImagenes(tag) {
     
 
     var cont = doc.getElementsByTagName(tag);
 
-    var largo = array.lenght;
+    var largo = imagenes.length;
 
-    if(num == largo){
+    if(contar > largo - 1){
         contar = 0;
     }
 
+    
+    cont[0].src = imagenes[contar];
 
-    cont[0].src = array[num];
-
-    editarContar(largo);
+    contar++;
 
 }
 
+function efectos(tag) {
+    
+    var cont = doc.getElementsByTagName(tag);
+
+    if (opacidad < 0) {
+        opacidad = 1;
+    }
+
+
+    console.log(opacidad);
+    cont[0].style = `opacity : ${opacidad}`;
+
+    opacidad = opacidad - 0.25;
+    
+}
