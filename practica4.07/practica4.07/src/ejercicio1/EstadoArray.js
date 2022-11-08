c7fa98e7fce35bf9ebe5d50213ee804407ff2e4e
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-11-08 19:59:04 +0100

    a

---
+++ practica4.07/practica4.07/src/ejercicio1/EstadoArray.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+
+
+function EstadoArray(props) {
+    
+
+    const estadoArrayInit = [];
+
+    const [numeros, setNumeros] = useState(estadoArrayInit);
+
+    const generarNumero = () => {
+
+        
+
+
+    }
+
+
+
+
+
+
+    return(
+
+        <React.Fragment>    
+
+        </React.Fragment>
+
+
+
+
+    );
+
+
+
+}
