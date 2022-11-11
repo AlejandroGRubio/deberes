b003b45cfa5357ce6d59558f9aa0c5f5eae7593c
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-11-11 19:57:52 +0100

    empezar2

--- practica4.08/practica4.08/src/ejercicio1/ListaCompra.js
+++ practica4.08/practica4.08/src/ejercicio1/ListaCompra.js
@@ -6,6 +6,9 @@ import React, { useState } from "react";
 function ListaCompra(props) {
     
 
+    const listaInicial = [];
+
+
 
 
 
@@ -14,7 +17,21 @@ function ListaCompra(props) {
     return (
 
         <React.Fragment>
+            <div>
 
+               <p>
+                   <label htmlFor={"nombrePro"}>Nombre: </label>
+                   <input type={"text"} name={"nombrePro"} id={"nombrePro"}></input>
+
+                   <label htmlFor={"descPro"}>Descripción: </label>
+
+               </p>
+
+
+            </div>
+            <div>
+
+            </div>
 
 
 
