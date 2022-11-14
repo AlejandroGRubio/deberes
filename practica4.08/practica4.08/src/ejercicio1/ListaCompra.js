ce9d3c0a7cb3b6d2b5f24e0019d79984350dcb0a
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-11-14 19:01:53 +0100

    medio

--- practica4.08/practica4.08/src/ejercicio1/ListaCompra.js
+++ practica4.08/practica4.08/src/ejercicio1/ListaCompra.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import * as bibliLista from "./biblioteca/bibliotecaLista.js";
 
 
 
@@ -7,8 +8,38 @@ function ListaCompra(props) {
     
 
     const listaInicial = [];
+    const valoresIniciales = {
+        id: "",
+        nombre: "",
+        descripcion: "",
+        precio: "",
+        unidades: "",
+    };
+
+    const [lista, setLista] = useState(listaInicial);
+    const [producto, setProducto] = useState(valoresIniciales);
 
 
+    const actDato = (e) =>{
+
+        const { name, value } = e.target;
+
+        setProducto({...producto, [name]: value});
+
+
+
+    }
+
+
+    function anyadirLista() {
+        
+        
+
+
+
+
+
+    }
 
 
 
@@ -21,14 +52,26 @@ function ListaCompra(props) {
 
                <p>
                    <label htmlFor={"nombrePro"}>Nombre: </label>
-                   <input type={"text"} name={"nombrePro"} id={"nombrePro"}></input>
-
-                   <label htmlFor={"descPro"}>Descripción: </label>
-
+                   <input type={"text"} name={"nombrePro"} onChange={actDato} className={''} value={producto.nombre}></input>
                </p>
+               <p>
+                   <p><label htmlFor={"descPro"}>Descripción: </label></p>
+                    
+                    <textarea name={"descPro"} rows="8" cols="35" onChange={actDato} className={''} value={producto.descripcion}></textarea>
+               </p>
+               <p>
+                    <label htmlFor={"precioPro"}>Precio: </label>
+                    <input type={"number"} name={"precioPro"} onChange={actDato} className={''} value={producto.precio}></input>
+               </p>
+               <p>
+                    <label htmlFor={"unidadesPro"}>Unidades: </label>
+                    <input type={"number"} name={"unidadesPro"} onChange={actDato} className={''} value={producto.unidades}></input>
+               </p>
+               <button id={"añadirButton"} onClick={() => {anyadirLista()}}>Añadir</button>
 
 
             </div>
+                
             <div>
 
             </div>
