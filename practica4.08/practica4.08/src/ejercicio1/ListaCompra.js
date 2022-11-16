b2cdc5557b1f80698683e1ddecd9bcdd2890ecda
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-11-16 17:25:28 +0100

    momento

--- practica4.08/practica4.08/src/ejercicio1/ListaCompra.js
+++ practica4.08/practica4.08/src/ejercicio1/ListaCompra.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import * as bibliLista from "./biblioteca/bibliotecaLista.js";
+import "./css/lista.css";
 
 
 
@@ -20,23 +21,47 @@ function ListaCompra(props) {
     const [producto, setProducto] = useState(valoresIniciales);
 
 
+    //Añade al producto segun el dato.
     const actDato = (e) =>{
 
         const { name, value } = e.target;
 
-        setProducto({...producto, [name]: value});
-
-
+        setProducto({...producto, [name]: value, id: bibliLista.generarId(5)});
 
     }
 
-
+    //Comprueba que el producto no esté vacio y si no es así lo añade.
     function anyadirLista() {
+
+        var error = false;
+
+        if (producto.nombre == `` || producto.descripcion == `` || producto.precio == `` || producto.unidades == ``) {
+            error = true;
+
+        }
+        else{
+            error = false;
+        }
+
+        if (error == false) {
+            setLista([...lista, producto]);
+        }
+       
+
+
+    }
+    //Da el valor inicial a la lista.
+    const borrarLista = () => {
+        setLista(listaInicial);
+    };
+    //Elimina un producto.
+    function eliminarProducto(codId, codNom, codDesc, codPrecio, codUni) {
         
+        const nuevosProd = lista.filter((producto) => {
+            return producto.id !== codId && producto.nombre !== codNom && producto.descripcion !== codDesc && producto.precio !== codPrecio && producto.unidades !== codUni;
+        });
         
-
-
-
+        setLista(nuevosProd);
 
 
     }
@@ -51,29 +76,38 @@ function ListaCompra(props) {
             <div>
 
                <p>
-                   <label htmlFor={"nombrePro"}>Nombre: </label>
-                   <input type={"text"} name={"nombrePro"} onChange={actDato} className={''} value={producto.nombre}></input>
+                   <label htmlFor={"nombre"}>Nombre: </label>
+                   <input type={"text"} name={"nombre"} onChange={actDato} className={'nombre'} value={producto.nombre}></input>
                </p>
                <p>
-                   <p><label htmlFor={"descPro"}>Descripción: </label></p>
+                   <p><label htmlFor={"descripcion"}>Descripción: </label></p>
                     
-                    <textarea name={"descPro"} rows="8" cols="35" onChange={actDato} className={''} value={producto.descripcion}></textarea>
+                    <textarea name={"descripcion"} rows="8" cols="35" onChange={actDato} className={'descripcion'} value={producto.descripcion}></textarea>
                </p>
                <p>
-                    <label htmlFor={"precioPro"}>Precio: </label>
-                    <input type={"number"} name={"precioPro"} onChange={actDato} className={''} value={producto.precio}></input>
+                    <label htmlFor={"precio"}>Precio: </label>
+                    <input type={"number"} name={"precio"} onChange={actDato} className={'precio'} value={producto.precio}></input>
                </p>
                <p>
-                    <label htmlFor={"unidadesPro"}>Unidades: </label>
-                    <input type={"number"} name={"unidadesPro"} onChange={actDato} className={''} value={producto.unidades}></input>
+                    <label htmlFor={"unidades"}>Unidades: </label>
+                    <input type={"number"} name={"unidades"} onChange={actDato} className={'unidades'} value={producto.unidades}></input>
                </p>
-               <button id={"añadirButton"} onClick={() => {anyadirLista()}}>Añadir</button>
+               <button onClick={() => {anyadirLista()}}>Añadir</button>
+               <button onClick={() => {borrarLista()}}>Borrar Todo</button>
 
 
             </div>
                 
             <div>
-
+                {lista[0] == undefined ? <p>No hay productos en la lista</p> : lista.map((listas) => {
+                    return <div className="producto" onClick={() => eliminarProducto(listas.id, listas.nombre, listas.descripcion, listas.precio, listas.unidades)}>
+                        <p>Id: {listas.id}</p>
+                        <p>Nombre: {listas.nombre}</p>
+                        <p>Descripción: {listas.descripcion}</p>
+                        <p>Precio: {listas.precio}</p>
+                        <p>Unidades: {listas.unidades}</p>  
+                    </div>
+                })}
             </div>
 
 
--- practica4.08/practica4.08/src/ejercicio1/biblioteca/bibliotecaLista.js
+++ practica4.08/practica4.08/src/ejercicio1/biblioteca/bibliotecaLista.js
@@ -2,7 +2,7 @@
 
 
 
-
+//Esta función al final no ha sido usada, pero la dejo por si en un futuro la utilizo, deberia decir si pasa la validación o no.
 export function comprobarDato(dato, validacion) {
     
     var errorExp = new RegExp(validacion);
@@ -16,15 +16,20 @@ export function comprobarDato(dato, validacion) {
     }
 
 }
-
+//Genera una un dato aleatorio, segun las letras que le indiques.
 export function generarId(numLetras){
 
 
-    var letras = [1, 2, 3 , 4, 5, 6, 7, 8, 9, `a`, `b`, `c`, `d`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
+    var letras = [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `a`, `b`, `c`, `d`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
 
-    var codId;
+    let codId = ``;
 
-    
+    for (let i = 0; i < numLetras; i++) {
+        
+        codId = `${codId}${letras[Math.floor(Math.random() * letras.length)]}`;
+    }
+
+    return codId;
 
 
 }
