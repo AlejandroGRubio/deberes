8c683ec34c6548d77bfffcd06db39624c30fb147
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-12-01 16:01:36 +0100

    a

--- practica5.07/practica5.07/src/App.js
+++ practica5.07/practica5.07/src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
-import PelisStarWars from './ejercicio1/PelisStarWars';
+import React, { useState, useEffect } from "react";
+import { getPeliculasYDatos } from "./ejercicio1/biblioteca/getDatosApi.js";
+import ListaPelis from "./ejercicio1/ListaPelis.js";
+import { peliculaSeleccionada } from './ejercicio1/biblioteca/seleccionDatos.js';
 
 function App() {
-  return (
-    <PelisStarWars/>
-  );
+
+  const url = "https://swapi.dev/api/films";
+
+    const valoresIniciales = [];
+    const peliInicial = {};
+
+    const [peliculas, setPeliculas] = useState(valoresIniciales);
+    const [pelicula, setPelicula] = useState(peliInicial);
+
+    
+
+    useEffect(()=>{
+
+        getPeliculasYDatos(url, setPeliculas);
+
+
+    },[])
+
+    console.log(peliculas);
+
+
+    return(
+
+      <React.Fragment>
+          <div className="contenedorStarWars">
+
+              
+              <ListaPelis peliculas={peliculas} funcion={peliculaSeleccionada}/>
+
+
+
+              
+              
+          </div>
+
+      </React.Fragment>
+    );
 }
 
 export default App;
--- practica5.07/practica5.07/src/ejercicio1/ListaPelis.js
+++ practica5.07/practica5.07/src/ejercicio1/ListaPelis.js
@@ -2,10 +2,52 @@ import React, { useState, useEffect } from "react";
 
 
 
-function ListaPelis(params) {
+function ListaPelis(props) {
     
 
 
+
+    return (
+
+        <React.Fragment>
+
+        <div id="divPelis" onClick = {(evento) =>{
+            if (evento.target.tagName == `LI`) {
+                props.funcion(evento.target.id);
+            }
+
+            }}>
+            <ul>
+
+                {props.peliculas.map((v,i,a) => {
+
+                    return (
+                        <li 
+                            key={i}
+                            className={`noClicked`}
+                            id={v.url}
+                        >
+                            {v.title}
+
+                        </li>
+                    )
+
+
+                    })
+            
+            
+                }
+
+
+
+            </ul>
+        </div>
+
+
+        </React.Fragment>
+
+
+    );
 
 
 
