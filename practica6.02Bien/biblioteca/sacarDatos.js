67c0a9b7ce75df6b2b2052085f4963907c116841
AlejandroGRubio <alejandrogarciarubio.alu@iespacomolla.es>
2022-12-22 01:43:40 +0100

    terminado practica 6.02

---
+++ practica6.02Bien/biblioteca/sacarDatos.js
@@ -0,0 +1,219 @@
+"use strict";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    getDoc,
+    onSnapshot,
+    doc,
+    query,
+    where,
+    orderBy,
+    limit,
+    updateDoc,
+    deleteDoc,
+    addDoc,
+  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { generarFormularioYDevolverDatos, mostrarDatos, mostrarDatosNombre, mostrarDatosPeso, mostrarDatosPrecio } from "./mostrarDatos.js";
+
+
+//Imprime los datos de la base de datos y los muestra.
+export const imprimirTodosLosDatos = async (db, nomBase, idUbi) => {
+
+
+  const productos = collection(db, nomBase);
+
+  const productosDocumentos = await getDocs(productos);
+
+  mostrarDatos(productosDocumentos, idUbi);
+
+}
+
+//Imprime los datos de la base de datos, según el filtro seleccionado.
+export const imprimirDatosFiltro = async (db, nomBase, idUbi, tipoFiltro) => {
+
+  const productos = collection(db, nomBase);
+
+  var doc = window.document;
+
+  var filtro = doc.getElementById(`formDatos`).value;
+
+
+  if (tipoFiltro == `Nombre`) {
+
+    const consulta = await query(productos, where(tipoFiltro, "==", filtro));
+
+    const productosDocumentos = await getDocs(consulta);
+
+    mostrarDatosNombre(productosDocumentos, idUbi);
+  }
+  else if (tipoFiltro == `Precio`) {
+
+    filtro = parseFloat(doc.getElementById(`formDatos`).value, 10);
+
+    const consulta = await query(productos, where(tipoFiltro, "<=", filtro));
+
+    const productosDocumentos = await getDocs(consulta);
+
+    mostrarDatosPrecio(productosDocumentos, idUbi);
+  }
+  else if (tipoFiltro == `Peso`) {
+
+    filtro = parseFloat(doc.getElementById(`formDatos`).value, 10);
+
+    const consulta = await query(productos, where(tipoFiltro, "<=", filtro));
+
+    const productosDocumentos = await getDocs(consulta);
+
+    mostrarDatosPeso(productosDocumentos, idUbi);
+  }
+
+
+}
+
+//Imprime los datos de la base de datos, según el orden seleccionado.
+export const imprimirDatosOrdenar = async(db, nomBase, idUbi, tipoOrden) => {
+
+  var doc = window.document;
+
+  const productos = collection(db, nomBase);
+
+  var ubi = doc.getElementById(idUbi);
+
+  ubi.innerHTML = ``;
+
+  if (tipoOrden == `Nombre`) {
+
+    const consulta = await query(productos, orderBy(tipoOrden));
+
+    const productosDocumentos = await getDocs(consulta);
+
+    mostrarDatos(productosDocumentos, idUbi);
+  }
+  else if (tipoOrden == `Precio`) {
+
+    const consulta = await query(productos, orderBy(tipoOrden));
+
+    const productosDocumentos = await getDocs(consulta);
+
+    mostrarDatos(productosDocumentos, idUbi);
+  }
+  else if (tipoOrden == `Peso`) {
+
+    const consulta = await query(productos, orderBy(tipoOrden));
+
+    const productosDocumentos = await getDocs(consulta);
+
+    mostrarDatos(productosDocumentos, idUbi);
+  }
+
+
+}
+
+//Va editando los datos que han sido cambiados.
+export const editarDatos = async (db, nomBase, idUbi, nuevosDatos) => {
+
+  const productos = collection(db, nomBase);
+
+  const editableRef = await doc(productos, idUbi);
+
+
+  if (nuevosDatos.Nombre != "") {
+    await updateDoc(editableRef, {Nombre: nuevosDatos.Nombre});
+  }
+
+  if (nuevosDatos.Descripcion != "") {
+    await updateDoc(editableRef, {Descripcion: nuevosDatos.Descripcion});
+  }
+
+  if (nuevosDatos.Precio != "") {
+    await updateDoc(editableRef, {Precio: nuevosDatos.Precio});
+  }
+
+  if (nuevosDatos.Peso != "") {
+    await updateDoc(editableRef, {Peso: nuevosDatos.Peso});
+  }
+
+  if (nuevosDatos.Imagen != "") {
+    await updateDoc(editableRef, {Imagen: nuevosDatos.Imagen});
+  }
+
+
+}
+
+//Borra el dato de la base de datos.
+export const borrarDato = async (db, nomBase, idUbi) => {
+
+  const productos = collection(db, nomBase);
+  
+  const borrado = await deleteDoc(doc(productos, idUbi));
+
+  var docu = window.document;
+
+  var infor = docu.createElement(`p`);
+
+  infor.innerText = `Recarga la página para dejar de mostrar el producto eliminado`;
+
+  docu.getElementById(idUbi).appendChild(infor);
+
+};
+
+
+//Añade un dato a la base de datos.
+export const anyadirALaBase = async (db, nomBase, nuevosDatos) => {
+
+  const productos = collection(db, nomBase);
+
+  const guardarNuevo = await addDoc(productos, nuevosDatos);
+
+};
+
+//Muestra cuantos productos hay en la base de datos.
+export const numProductos = async (db, nomBase, idUbi) => {
+
+
+  var docu = window.document;
+
+  const productos = collection(db, nomBase);
+
+  const productosDocumentos = await getDocs(productos);
+
+  var numP = productosDocumentos.docs.length;
+
+
+  var infor = docu.createElement(`p`);
+
+  infor.innerText = `Actualmente hay ${numP} productos`;
+
+  docu.getElementById(idUbi).appendChild(infor);
+
+}
+
+//Muestra la media de los precios de la base de datos.
+export const mediaPrecioProductos = async (db, nomBase, idUbi) => {
+
+
+  var docu = window.document;
+
+  const productos = collection(db, nomBase);
+
+  const productosDocumentos = await getDocs(productos);
+
+  var numP = productosDocumentos.docs.length;
+
+  var precioTotal = 0;
+
+
+  productosDocumentos.docs.map((docus) => {
+
+    precioTotal = precioTotal + parseFloat(docus.data().Precio);
+
+
+  });
+  var infor = docu.createElement(`p`);
+
+  infor.innerText = `La media de los precios de los productos son ${precioTotal / numP}`;
+
+  docu.getElementById(idUbi).appendChild(infor);
+
+}
---
+++ practica6.02Bien/ejercicio1.js
@@ -0,0 +1,154 @@
+"use strict";
+import { app } from "./biblioteca/conexion.js";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    getDoc,
+    onSnapshot,
+    doc,
+    query,
+    where,
+    orderBy,
+    limit,
+  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { borrarDato, imprimirDatosFiltro, imprimirDatosOrdenar, imprimirTodosLosDatos, mediaPrecioProductos, numProductos } from "./biblioteca/sacarDatos.js";
+import { anyadirNuevosDatos, generarFormularioYDevolverDatos } from "./biblioteca/mostrarDatos.js";
+
+window.onload = () => {
+
+  var doc = window.document;
+
+  const db = getFirestore(app);
+
+  //Imprime los datos de la base de datos al cargar la página.
+  imprimirTodosLosDatos(db, "productos", "allProducts");
+
+  //Añade un evento al botón Buscar.
+  doc.getElementById(`buscarDatos`).addEventListener(`click`, () => {
+
+    doc.getElementById(`allProducts`).className = `oculto`;
+
+    if (doc.getElementById(`nombre`).checked == true) {
+      imprimirDatosFiltro(db, `productos`, `productsFilter`, `Nombre`);
+    }
+    else if (doc.getElementById(`precio`).checked == true) {
+      imprimirDatosFiltro(db, `productos`, `productsFilter`, `Precio`);
+    }
+    else if(doc.getElementById(`peso`).checked == true){
+      imprimirDatosFiltro(db, `productos`, `productsFilter`, `Peso`);
+    }
+
+  });
+
+  //Añade el evento a los botones de Ordenar Por.
+  doc.getElementById(`ordenar`).addEventListener(`click`, (e) => {
+
+    if (e.target.tagName == `BUTTON`) {
+
+      doc.getElementById(`allProducts`).className = `oculto`;
+
+      if (e.target.id == `ordenarNombre`) {
+        imprimirDatosOrdenar(db, `productos`, `productsFilter`, `Nombre`);
+      }
+      else if (e.target.id == `ordenarPrecio`) {
+        imprimirDatosOrdenar(db, `productos`, `productsFilter`, `Precio`);
+      }
+      else if(e.target.id == `ordenarPeso`){
+        imprimirDatosOrdenar(db, `productos`, `productsFilter`, `Peso`);
+      }
+    }
+  });
+
+  //Añade el evento al botón Restablecer Datos.
+  doc.getElementById(`restaurarDatos`).addEventListener(`click`, () => {
+
+    doc.getElementById(`productsFilter`).innerHTML = ``;
+    
+    
+    doc.getElementById(`allProducts`).className = ``;
+
+  });
+    
+
+  doc.getElementById(`allProducts`).addEventListener(`click`, (e) => {
+
+
+    if (e.target.tagName == `BUTTON`) {
+      
+    
+        if (e.target.innerText == `Editar`) {
+          generarFormularioYDevolverDatos(db, `productos`, e.target.parentNode.id);
+        }else if(e.target.innerText == `Eliminar`){
+          borrarDato(db, `productos`, e.target.parentNode.id);
+        }
+    
+    }
+
+  });
+
+  doc.getElementById(`productsFilter`).addEventListener(`click`, (e) => {
+
+
+    if (e.target.tagName == `BUTTON`) {
+      
+    
+        if (e.target.innerText == `Editar`) {
+
+          if (doc.getElementById(`allProducts`) != undefined) {
+            doc.getElementById(`allProducts`).remove();
+          }
+          generarFormularioYDevolverDatos(db, `productos`, e.target.parentNode.id);
+        }else if(e.target.innerText == `Eliminar`){
+          if (doc.getElementById(`allProducts`) != undefined) {
+            doc.getElementById(`allProducts`).remove();
+          }
+          borrarDato(db, `productos`, e.target.parentNode.id);
+        }
+    
+    }
+
+  });
+
+
+  doc.getElementById(`botonMostrarAnyadirDatos`).addEventListener(`click`, () => {
+
+    if (doc.getElementById(`anyadirDatos`).className == `mostrarAnyadirDatos`) {
+      doc.getElementById(`anyadirDatos`).className = `oculto`;
+    }
+    else if(doc.getElementById(`anyadirDatos`).className == `oculto`){
+      doc.getElementById(`anyadirDatos`).className = `mostrarAnyadirDatos`;
+    }
+
+  })
+
+
+  doc.getElementById(`anyadirDatos`).addEventListener(`click`, (e) => {
+
+
+    if (e.target.tagName == `BUTTON`) {
+      
+
+      if (e.target.innerText == `Guardar`) {
+        anyadirNuevosDatos(db, `productos`, e.target.parentNode.id);
+      }
+
+
+
+
+    }
+
+
+  });
+
+
+  numProductos(db, `productos`, `infoNumProduc`);
+
+
+  mediaPrecioProductos(db, `productos`, `precioMedioProduc`);
+
+
+
+
+
+} //Fin de onload.
